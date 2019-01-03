b059e52413b141a4199436d89e1430cd306bbaaf
Anders Juul <andersjuulsfirma@gmail.com>
2019-01-03 22:18:39 +0100

    rewired start object

--- Src/Ajf.NsPlanner3.App/App.xaml.cs
+++ Src/Ajf.NsPlanner3.App/App.xaml.cs
@@ -1,11 +1,17 @@
-﻿using System.Windows;
+﻿using Ajf.NsPlanner3_App;
 
 namespace Ajf.NsPlanner3.App
 {
     /// <summary>
-    /// Interaction logic for App.xaml
+    ///     Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        public App()
+        {
+            var view = new MainWindow();
+
+            view.ShowDialog();
+        }
     }
 }
