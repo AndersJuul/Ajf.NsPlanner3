e8380926ed2cac53296d9ff8f617f276662daef9
Anders Juul <andersjuulsfirma@gmail.com>
2019-01-03 21:42:43 +0100

    ns

--- Src/Ajf.NsPlanner3.App/App.xaml.cs
+++ Src/Ajf.NsPlanner3.App/App.xaml.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
-namespace Ajf.NsPlanner3_App
+namespace Ajf.NsPlanner3.App
 {
     /// <summary>
     /// Interaction logic for App.xaml
