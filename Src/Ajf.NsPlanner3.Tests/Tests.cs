4b8227c6e59dce47c69852a0726a05ce4673c994
Anders Juul <andersjuulsfirma@gmail.com>
2019-01-03 23:11:31 +0100

    Added test lib

---
+++ Src/Ajf.NsPlanner3.Tests/Tests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Ajf.NsPlanner3.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void That()
+        {
+        }
+    }
+}
